f4daa6993460bcae2406f2424c72d3aadcc25c8c
isaacwallace123 <goosewal@gmail.com>
2024-10-28 23:21:54 -0400

    changed format for all currencies

--- src/utils/FormatCurrency.ts
+++ src/utils/FormatCurrency.ts
@@ -8,10 +8,10 @@ const Currencies = {
     bank: '<:bank:1300519780578758808>'
 };
 
-const FormatBalance = (amount: number) => `**${Currencies.balance}${Suffix(amount)}**`;
+const FormatBalance = (amount: number) => `**${Currencies.balance} ${Suffix(amount)}**`;
 
-const FormatCrypto = (amount: number) => `**${Currencies.crypto}${Suffix(amount)}**`;
+const FormatCrypto = (amount: number) => `**${Currencies.crypto} ${Suffix(amount)}**`;
 
-const FormatBank = (balance: number, level: number) => `**${Currencies.bank}${Suffix(balance)} / ${Suffix(getMaxBalance(level))}**`;
+const FormatBank = (balance: number, level: number) => `**${Currencies.bank} ${Suffix(balance)} / ${Suffix(getMaxBalance(level))}**`;
 
 export { Currencies, FormatBalance, FormatCrypto, FormatBank };
